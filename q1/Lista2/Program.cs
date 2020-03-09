d6074d484ea4e1a582c77c18cc8b31d48abee17f
Gabriel Pereira Coelho De Oliveira <oliveira.coelho@academico.ifrn.edu.br>
2020-03-09 15:02:31 -0300

    questao 1

---
+++ q1/Lista2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > y) Console.WriteLine($"Maior:{x}");
+                    
+            else if (y > x) Console.WriteLine($"Maior:{y}");
+                    
+            else Console.Write("Números iguais");
+                    
+            Console.ReadKey();
+        }
+    }
+}
